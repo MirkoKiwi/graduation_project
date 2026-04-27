f52adac757abe7ceae206a335fe090bb31129381
MirkoKiwi <106871665+MirkoKiwi@users.noreply.github.com>
2026-04-27 22:52:09 +0200

    feat: loop iteration in main

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ from utils.helpers import ensure_ollama_is_running
 
 def main():
     ensure_ollama_is_running()
-    model_name = os.getenv("MODEL_NAME")
 
-    user_in = input("Topic: ")
+    while True:
+        user_in = input("(Type /bye to quit) Topic: ")
+        if user_in.lower() == "/bye":
+            break
 
-    timestamp = int(time.time())
+        timestamp = int(time.time())
 
-    print("\33[44m[*]\33[0m Generating...")
-    raw_output  = get_llm_references(user_in)
-    
-    bib_filename = f"refs_{timestamp}.bib"
-    bib_path     = save_bibtex(raw_output, bib_filename)
+        print("\33[44m[*]\33[0m Generating...")
+        raw_output  = get_llm_references(user_in)
+        
+        bib_filename = f"refs_{timestamp}.bib"
+        bib_path     = save_bibtex(raw_output, bib_filename)
 
-    print(f"\33[44m[+]\33[0m BibTeX saved at: {bib_path}")
-    
-    print(f"\n\33[0m--- \33[32mCOMPLETED \33[0m---")
+        print(f"\33[44m[+]\33[0m BibTeX saved at: {bib_path}")
+        
+        print(f"\n\33[0m--- \33[32mCOMPLETED \33[0m---")
 
 
 
